20e703103475ef54d4fb1bc3b3a1af02c8d443e5
Shivanktyagi07 <shivanktyagi976@gmail.com>
2023-12-19 21:46:59 +0530

    adding navbar

--- script.js
+++ script.js
@@ -40,3 +40,35 @@ document.addEventListener("mousemove", function (dets) {
     });
   }
   loadinganimation();
+
+ 
+function cursorAnimation() {
+  document.addEventListener("mousemove", function (dets) {
+    gsap.to("#cursor", {
+      left: dets.x,
+      top: dets.y,
+    });
+  });
+  // document.querySelector("#child1").addEventListener("mouseenter",function(){
+
+  // })
+
+  // document.querySelector("#child1").addEventListener("mouseleave",function(){
+  //   gsap.to("#cursor",{
+  //     transform: 'translate(-50%,-50%) scale(0)'
+  //   })
+  // })
+  document.querySelectorAll(".child").forEach(function (elem) {
+    elem.addEventListener("mouseenter", function () {
+      gsap.to("#cursor", {
+        transform: "translate(-50%,-50%) scale(1)",
+      });
+    });
+    elem.addEventListener("mouseleave", function () {
+      gsap.to("#cursor", {
+        transform: "translate(-50%,-50%) scale(0)",
+      });
+    });
+  });
+}
+cursorAnimation();
